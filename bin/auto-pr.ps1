28b3da668a0b9730b64d1e83c08bb0da41fbf80f
Arama120517 <arama.dev@outlook.com>
2026-05-03 21:43:32 +0800

    fix: 修复ci

--- bin/auto-pr.ps1
+++ bin/auto-pr.ps1
@@ -1,6 +1,6 @@
 param(
     # overwrite upstream param
-    [String]$upstream = "<username>/<bucketname>:main"
+    [String]$upstream = "Arama120517/scoop-proxy:main"
 )
 
 if (!$env:SCOOP_HOME) { $env:SCOOP_HOME = Convert-Path (scoop prefix scoop) }
